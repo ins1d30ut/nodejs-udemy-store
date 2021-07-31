904ace659f9075517f5591b51bf74b69e817114b
ins1d30ut <bmoses001@protonmail.com>
2021-07-31 04:47:26 -0500

    commit add img and data

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,19 +1,23 @@
-const Product = require('../models/product');
+const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
-  res.render('admin/add-product', {
-    pageTitle: 'Add Product',
-    path: '/admin/add-product',
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true
-  });
+	res.render("admin/add-product", {
+		pageTitle: "Add Product",
+		path: "/admin/add-product",
+		formsCSS: true,
+		productCSS: true,
+		activeAddProduct: true,
+	});
 };
 
 exports.postAddProduct = (req, res, next) => {
-  const product = new Product(req.body.title);
-  product.save();
-  res.redirect('/');
+	const title = req.body.title;
+	const imageUrl = req.body.imageUrl;
+	const price = req.body.price;
+	const description = req.body.description;
+	const product = new Product(title, imageUrl, price, description);
+	product.save();
+	res.redirect("/");
 };
 
 exports.getProducts = (req, res, next) => {
@@ -21,7 +25,7 @@ exports.getProducts = (req, res, next) => {
 		res.render("admin/products", {
 			prods: products,
 			pageTitle: "Admin Products",
-      path: '/admin/products'
+			path: "/admin/products",
 		});
 	});
-}
+};
--- models/product.js
+++ models/product.js
@@ -1,9 +1,28 @@
 const fs = require('fs');
 const path = require('path');
 
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
+
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
+
 module.exports = class Product {
-  constructor(t) {
-    this.title = t;
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
   }
 
   save() {
